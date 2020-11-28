caa37c8390431411437608093f37de4b7a732f3e
yhaidai <gayday57@gmail.com>
2020-11-28 20:42:23 +0200

    final version

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
 var area = document.getElementById('area'),
-    move = 1, volume = 1, cross = 0, zero = 0;
+    move = 1, volume = 1, victory = false;
+
 for (var i = 0; i < 9; i++)
     area.innerHTML+='<div class="cell"></div>';
+
+if (sessionStorage.getItem('zero') == null)
+{
+    sessionStorage.setItem('zero', 0);
+    sessionStorage.setItem('cross', 0);
+}
+
+document.getElementById('zero').innerHTML = "Нолики: " + sessionStorage.getItem('zero');
+document.getElementById('cross').innerHTML = "Крестики: " + sessionStorage.getItem('cross');
+
 area.addEventListener('click', (event) => {
-    if (event.target.innerHTML == '')
+    if (event.target.innerHTML == '' && !victory)
     {
         if (move % 2 === 0){
             event.target.innerHTML = 'O';
             document.getElementById('text').innerHTML = "х КРЕСТИКИ х";
             check('O');
+            if(victory)
+            {
+                document.getElementById('text').innerHTML = "Победили нолики!";
+                document.getElementById('zero').innerHTML = "Нолики: " + sessionStorage.getItem('zero');
+                ;
+            }
         }
         else{
            event.target.innerHTML = 'X'; 
            document.getElementById('text').innerHTML = "o НОЛИКИ о"
            check('X');
+           if(victory)
+           {
+                document.getElementById('text').innerHTML = "Победили крестики!";
+                document.getElementById('cross').innerHTML = "Крестики: " + sessionStorage.getItem('cross');
+           }
         }
         move++;
         if (volume++ == 9)
-            alert('draw');
+            document.getElementById('text').innerHTML = "Ничья!";
     } 
 })
 
@@ -42,8 +64,25 @@ function check(alpha)
     if (v[0] == 3 || v[1] == 3 || v[2] == 3|| h[0] == 3 || 
         h[1] == 3 || h[2] == 3 || d[0] == 3 || d[1] == 3)
         {
-            alert('vic');
-            if (alpha == 'O') zero++;
-            else cross++;
+            victory = true;
+            if (alpha == 'O'){
+                zero = sessionStorage.getItem('zero');
+                zero++;
+                sessionStorage.setItem('zero', zero);
+            } 
+            else {
+                cross = sessionStorage.getItem('cross');
+                cross++;
+                sessionStorage.setItem('cross', cross);;
+            }
         }
 }
+
+ function start()
+ {
+    var cells = document.getElementsByClassName('cell');
+    victory = false;
+    foreach (cell in cells)
+        cell.innerHTML = '';
+ }
+
